3414a49bbd92f50ccd6bdf7f6e2dddcc9ec4788b
Jorge Saucedo <jsaucedo.developer@gmail.com>
2019-01-10 20:28:27 -0500

    added media queries and made header and gallery more responsive

---
+++ dist/js/main.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector('.menu-toggle');
+const menuNav = document.querySelector('.menu-nav');
+const menuItems = document.querySelectorAll('.nav-item');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+let btnToggled = false;
+function toggleMenu(){
+    if (!btnToggled) {
+        menuBtn.classList.add('close');
+        menuNav.classList.add('show');
+        menuItems.forEach(item => item.classList.add('show'));
+        btnToggled = true;
+        console.log('works!');
+    } else {
+        menuBtn.classList.remove('close');
+        menuNav.classList.remove('show');
+        menuItems.forEach(item => item.classList.remove('show'));
+        btnToggled = false;
+    }
+}
